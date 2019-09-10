8518d4d7c85458841d376ebd2d0502b2cd73a92f
therealplato <therealplato@gmail.com>
2019-09-10 13:37:43 -0400

    implement validation of edge directions

--- parse.go
+++ parse.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"regexp"
 )
 
 func parseInput(r io.Reader) ([]node, error) {
@@ -24,11 +25,21 @@ func parseInput(r io.Reader) ([]node, error) {
 	return out, nil
 }
 
+// Assumption: city names may not have equals signs
+// Assumption: directions are lowercase
+var regexpDirection = regexp.MustCompile(`^(north|east|south|west)=([^=]+)$`)
+
 func parseLine(bb []byte) (node, error) {
 	var n node
 	fields := bytes.Fields(bb)
 	if len(fields) > 5 {
 		return n, fmt.Errorf("input had too many items: %q", string(bb))
+	}
+	for i := 1; i < len(fields); i++ {
+		groups := regexpDirection.FindSubmatch(fields[i])
+		if len(groups) != 2 {
+			return n, fmt.Errorf("input direction did not match north=Beirut: %q", string(fields[i]))
+		}
 	}
 	return n, nil
 }
