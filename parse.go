74fd5a38cb6aad49ff57b43d5390a8ade1b117e5
therealplato <therealplato@gmail.com>
2019-09-10 13:28:25 -0400

    iterate input lines and fields

--- parse.go
+++ parse.go
@@ -1,7 +1,34 @@
 package grayongrey
 
-import "io"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
 
-func parseInput(io.Reader) ([]node, error) {
-	return nil, nil
+func parseInput(r io.Reader) ([]node, error) {
+	var out []node
+	bb, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	lines := bytes.Split(bb, []byte("\n"))
+	for _, line := range lines {
+		n, err := parseLine(line)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, n)
+	}
+	return out, nil
+}
+
+func parseLine(bb []byte) (node, error) {
+	var n node
+	fields := bytes.Fields(bb)
+	if len(fields) > 5 {
+		return n, fmt.Errorf("input had too many items: %q", string(bb))
+	}
+	return n, nil
 }
--- parse_test.go
+++ parse_test.go
@@ -6,9 +6,18 @@ import (
 )
 
 func TestParseInputErrors(t *testing.T) {
-	input := bytes.NewBufferString("a up=ISS")
-	_, err := parseInput(input)
-	if err == nil {
-		t.Fatal("expected parse error, got nil")
-	}
+	t.Run("invalid direction", func(t *testing.T) {
+		input := bytes.NewBufferString("a up=ISS")
+		_, err := parseInput(input)
+		if err == nil {
+			t.Fatal("expected parse error, got nil")
+		}
+	})
+	t.Run("extra directions", func(t *testing.T) {
+		input := bytes.NewBufferString("a north=b east=c south=d west=e up=ISS")
+		_, err := parseInput(input)
+		if err == nil {
+			t.Fatal("expected parse error, got nil")
+		}
+	})
 }
