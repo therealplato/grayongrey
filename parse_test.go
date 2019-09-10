be3a047d8b60bb598fa55aba48164e749d46f964
therealplato <therealplato@gmail.com>
2019-09-10 15:00:48 -0400

    re-add test for implicit node creation

--- parse_test.go
+++ parse_test.go
@@ -62,5 +62,23 @@ Beirut
 }
 
 func TestParseInputPopulatesMissingNodes(t *testing.T) {
-	t.Skip("Assumption: Every city has a line present in the input")
+	input := bytes.NewBufferString("Athens north=Beirut")
+	nodes, names, err := parseInput(input)
+	if err != nil {
+		t.Fatalf("expected nil error, got %v", err)
+	}
+	if len(nodes) != 2 {
+		t.Fatalf("expected two nodes, got %v", len(nodes))
+	}
+	_, ok := nodes["Athens"]
+	if !ok {
+		t.Fatal("expected node map keyed on Athens, but was missing")
+	}
+	_, ok = nodes["Beirut"]
+	if !ok {
+		t.Fatal("expected node map keyed on Athens, but was missing")
+	}
+	if len(names) != 2 {
+		t.Fatalf("expected two names, got %v", len(names))
+	}
 }
